953277d591916c666ae152c252b8e3a9a38a8761
Karsten Silkenbäumer <kassi@users.noreply.github.com>
2018-07-25 22:58:26 +0200

    Ignore storage folder

--- storage/configuration/key.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-// Do not edit or remove this file unless advised by Koken support
-// support@koken.me
-
-return '984dd4d6ce39e5bca7654f93470902e3';
